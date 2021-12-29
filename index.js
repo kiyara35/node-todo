1248d287623d3173b2f9e0182f223e1aaea66afc
kiyara <kiyaraeldiyarova@gmail.com>
2021-12-29 00:32:34 -0300

    sixteen commit

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const cors = require('cors')
 
 const app = express()
 
-var port = process.env.PORT || 5000
-app.listen(port, ()=>{
+var port = 5000
+app.listen(port || process.env.PORT, ()=>{
     console.log('server started')
 })
 
