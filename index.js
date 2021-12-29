30a72463225475a1c936b475cf8da38238d3c6d6
kiyara <kiyaraeldiyarova@gmail.com>
2021-12-29 00:11:33 -0300

    twelve commit

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const path = require('path')
-const PORT = process.env.PORT || 5000
 const cors = require('cors')
 
 
 const app = express()
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
     console.log('server started')
 })
+
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
